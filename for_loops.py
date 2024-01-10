39b310fbef9018d60ad1377f7cbb4b13d0c2f082
shirinyamani <yamani.shirin@ucalgary.ca>
2024-01-10 14:45:53 -0700

    for loop and function added

---
+++ for_loops.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+products = [
+    {"name": "Laptop", "category": "Electronics", "price": 999.99},
+    {"name": "Smartphone", "category": "Electronics", "price": 699.99},
+    {"name": "Book: The Alchemist", "category": "Books", "price": 14.99},
+    {"name": "Book: Harry Potter", "category": "Books", "price": 29.99},
+    {"name": "Bluetooth Headphones", "category": "Electronics", "price": 199.99},
+    {"name": "Monitor", "category": "Electronics", "price": 149.99},
+    {"name": "Book: Python Programming", "category": "Books", "price": 49.99},
+    {"name": "Desk Lamp", "category": "Furniture", "price": 24.99},
+    {"name": "Mousepad", "category": "Accessories", "price": 5.99},
+    {"name": "Stylus Pen", "category": "Accessories", "price": 12.99},
+]
+
+
+# 1. Generate a list of product names that are in the category "Electronics".
+electronic_items = [product["name"] for product in products if product["category"] == "Electronics"]
+print("electronic items:", electronic_items)
+
+
+
+# %%
+# 3. Compute the average price of all the products in the "Books" category.
+books_prices = [product["price"] for product in products if product["category"] == "Books"]
+avrg = sum(books_prices) / len(books_prices) if books_prices else 0
+print(avrg)
+# %%
